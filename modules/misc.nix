e62fd082fdc57d9795369cbd5b1b6ec30e73312c
Alexander Sieg <alex@xanderio.de>
2022-03-13 16:00:17 +0100

    install ripgrep via nix

--- modules/misc.nix
+++ modules/misc.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    ripgrep
+  ];
+
   programs.direnv.enable = true;
   programs.exa.enable = true;
+  programs.jq.enable = true;
   programs.bat = {
     enable = true;
     config = {
