ca07bc040fbbc87fbdc084c34cefc27c5711e35b
Alexander Sieg <alex@xanderio.de>
2022-03-18 18:06:41 +0100

    install gnome-icon-theme

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -66,6 +66,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     swaylock
+    gnome-icon-theme
     gnome3.adwaita-icon-theme
     gnomeExtensions.appindicator
   ];
