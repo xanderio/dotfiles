eac9f230df24b7116a0560b95989061a96868929
Alexander Sieg <alex@xanderio.de>
2022-04-07 11:41:48 +0200

    add virtual loopback camera

--- vger/boot.nix
+++ vger/boot.nix
@@ -1,10 +1,26 @@
-{ ... }:
+{ config, ... }:
 {
-  boot.cleanTmpDir = true;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.availableKernelModules = [
-    "aesni_intel"
-    "cryptd"
-  ];
+  boot = {
+    cleanTmpDir = true;
+    extraModulePackages = with config.boot.kernelPackages;
+      [ v4l2loopback.out ];
+    kernelModules = [
+      # Virtual Camara
+      "v4l2loopback"
+    ];
+    extraModprobeConfig = ''
+      # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
+      # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
+      # https://github.com/umlaeute/v4l2loopback
+      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+    '';
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    initrd.availableKernelModules = [
+      "aesni_intel"
+      "cryptd"
+    ];
+  };
 }
