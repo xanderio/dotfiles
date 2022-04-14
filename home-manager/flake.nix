33f46f3b8d5b79edfb549c264f950c9cb51531ed
Alexander Sieg <alex@xanderio.de>
2022-04-14 15:11:15 +0200

    automatic task sync

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -16,14 +16,8 @@
 
   outputs = { self, home-manager, ... }@inputs:
     let
-      firefox-overlay = final: prev: {
-        firefox-wayland = (import inputs.nixpkgs-staging-next {
-          system = prev.system;
-        }).pkgs.firefox-wayland;
-      };
       overlays = [
         inputs.neovim-nightly-overlay.overlay
-        firefox-overlay
         (final: prev: {
           nvim-ts-grammars = prev.callPackage ./pkgs/nvim-ts-grammars { };
           timewarrior-hook = prev.callPackage ./pkgs/timewarrior-hook.nix { };
--- home-manager/modules/taskwarrior.nix
+++ home-manager/modules/taskwarrior.nix
@@ -3,6 +3,34 @@
   programs = {
     taskwarrior = {
       enable = true;
+      colorTheme = "dark-256";
+      extraConfig = ''
+        include ~/.config/task/localrc
+      '';
+    };
+  };
+
+  systemd.user = {
+    services.task-sync = {
+      Unit = {
+        Description = "sync taskwarrior";
+      };
+      Service = {
+        ExecStart = "${pkgs.taskwarrior}/bin/task sync";
+      };
+    };
+
+    timers.task-sync = {
+      Unit = {
+        Description = "sync taskwarrior";
+      };
+
+      Install.WantedBy = [ "timers.target" ];
+
+      Timer = {
+        Unit = "task-sync.service";
+        OnActiveSec = "10m";
+      };
     };
   };
 
