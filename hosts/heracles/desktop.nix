0fe3d10b2836affa67ed4e778fd092c820871c20
Alexander Sieg <alex@xanderio.de>
2022-04-21 16:28:12 +0200

    init heracles

---
+++ hosts/heracles/desktop.nix
@@ -0,0 +1,52 @@
+{pkgs, ...}: {
+  programs = {
+    sway.enable = true;
+    fish.enable = true;
+    dconf.enable = true;
+    file-roller.enable = true;
+    gnome-disks.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    swaylock
+    gnome-icon-theme
+    hicolor-icon-theme # default fallback for icons
+    gnome3.adwaita-icon-theme
+    gnome.gnome-themes-extra
+
+    gnomeExtensions.appindicator
+
+    gnome.gnome-system-monitor
+    gnome.nautilus
+  ];
+
+  gtk.iconCache.enable = true;
+
+  services = {
+    udev.packages = with pkgs; [gnome3.gnome-settings-daemon];
+    gvfs.enable = true;
+    gnome = {
+      gnome-keyring.enable = true;
+      sushi.enable = true;
+    };
+
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+    };
+  };
+
+  xdg.portal.wlr = {
+    enable = true;
+    settings = {
+      screencast = {
+        exec_before = "${pkgs.mako}/bin/makoctl set-mode do-not-disturb";
+        exec_after = "${pkgs.mako}/bin/makoctl set-mode default";
+        chooser_type = "simple";
+        chooser_cmd = "''${pkgs.slurp}/bin/slurp -f %o -or";
+      };
+    };
+  };
+}
---
+++ hosts/heracles/firewall.nix
@@ -0,0 +1,7 @@
+{...}: {
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+}
