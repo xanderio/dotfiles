a53f1c84221bce26c39ce2784ced022ffb4f1925
Alexander Sieg <alex@xanderio.de>
2022-04-27 13:38:40 +0200

    install indent-blankline.nvim

--- modules/neovim/nvim/lua/plugins.lua
+++ modules/neovim/nvim/lua/plugins.lua
@@ -242,6 +242,15 @@ return require('packer').startup(function()
   -- }}}
   -- Utils {{{
   use {
+    'lukas-reineke/indent-blankline.nvim',
+    config = function()
+      require("indent_blankline").setup {
+        show_current_context = true,
+        show_current_context_start = true,
+      }
+    end
+  }
+  use {
     'junegunn/vim-easy-align',
     config = function()
       vim.cmd [[ xmap <leader>a <Plug>(EasyAlign) ]]
