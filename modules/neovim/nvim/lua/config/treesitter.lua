0f5b32057621cc844d41e94bee19628d13e99532
Alexander Sieg <alex@xanderio.de>
2022-05-09 18:22:05 +0200

    treesitter

--- modules/neovim/nvim/lua/config/treesitter.lua
+++ modules/neovim/nvim/lua/config/treesitter.lua
@@ -1,9 +1,65 @@
 require('nvim-treesitter.configs').setup({
-  highlight = { enable = true },
-  incremental_selection = { enable = true },
-  textobjects = { enable = true },
-  indent = { enable = true }
+  highlight = {
+    enable = true 
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+  },
+  indent = {
+    enable = true,
+    disable = {
+      "python",
+      "yaml"
+    }
+  }
 })
 
-vim.api.nvim_set_keymap('o', 'm', '<cmd>lua require("tsht").nodes()<CR>', {silent=true})
-vim.api.nvim_set_keymap('v', 'm', '<cmd>lua require("tsht").nodes()<CR>', {noremap=true, silent=true})
+vim.keymap.set('o', 'm',
+  function()
+    require("tsht").nodes()
+  end
+) 
+vim.keymap.set('v', 'm',
+  function()
+    require("tsht").nodes()
+  end
+) 
