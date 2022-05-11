7b80e8db5924cf83d386ee02a0e1f9838729e227
Alexander Sieg <alex@xanderio.de>
2022-05-11 10:59:23 +0200

    sshd on laptops

--- configuration/laptop/services.nix
+++ configuration/laptop/services.nix
@@ -8,11 +8,26 @@
   services.avahi = {
     enable = true;
     nssmdns = true;
+    ipv4 = true;
+    ipv6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
   };
   services.pcscd = {
     enable = true;
     plugins = [pkgs.ifdnfc];
   };
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    permitRootLogin = "without-password";
+  };
+  networking.firewall.allowedTCPPorts = [22];
 
   virtualisation.docker.enable = true;
   virtualisation.podman.enable = true;
--- configuration/laptop/users.nix
+++ configuration/laptop/users.nix
@@ -4,5 +4,9 @@
     isNormalUser = true;
     extraGroups = ["wheel" "networkmanager" "audio" "docker" "adbusers"]; # Enable ‘sudo’ for the user.
     shell = pkgs.fish;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJDvsq3ecdR4xigCpOQVfmWZYY74KnNJIJ5Fo0FsZMGW" # vger
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBWdll21KPn3xuWFM+TnzdSRABU+0zVHi3nmCAzt/1QR" # hercules
+    ];
   };
 }
