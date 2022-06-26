ea035afbfeb1d74e08040cef76c55d80e39d594b
Alexander Sieg <alex@xanderio.de>
2022-06-26 11:08:53 +0200

    set hid_apple fnmode for keychron to work properbly

--- configuration/laptop/boot.nix
+++ configuration/laptop/boot.nix
@@ -11,6 +11,8 @@
       # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
       # https://github.com/umlaeute/v4l2loopback
       options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+
+      options hid_apple fnmode=2
     '';
     loader = {
       systemd-boot.enable = true;
