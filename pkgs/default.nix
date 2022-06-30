ed50ee671d3ff43d997badb6cdd57515c15a2382
Alexander Sieg <alex@xanderio.de>
2022-06-30 13:57:31 +0200

    remove pixelfed

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,5 @@
 {}: final: prev: {
   nvim-ts-grammars = prev.callPackage ./nvim-ts-grammars { };
   timewarrior-hook = prev.callPackage ./timewarrior-hook { };
-  pixelfed = prev.callPackage ./pixelfed { };
   ferium = prev.callPackage ./ferium { };
 }
