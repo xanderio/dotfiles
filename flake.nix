3f543730ab19462917e42f5c9fb71d336003c0cf
Alexander Sieg <alex@xanderio.de>
2022-07-24 09:26:33 +0200

    update

--- flake.nix
+++ flake.nix
@@ -19,51 +19,56 @@
       url = "github:Infinidoge/nix-minecraft";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    polymc.url = "github:PolyMC/PolyMC";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixos-hardware,
-    home-manager,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
+  outputs =
+    { self
+    , nixpkgs
+    , nixos-hardware
+    , home-manager
+    , ...
+    } @ inputs:
+    let
+      system = "x86_64-linux";
 
-    overlays = import ./overlays {
-      inherit pkgs inputs;
-    };
-
-    pkgs = import nixpkgs {
-      inherit system overlays;
-      config.allowUnfree = true;
-    };
-    lib = import ./lib {inherit nixpkgs home-manager pkgs overlays system inputs;};
-  in {
-    devShells."${system}".default = pkgs.mkShellNoCC {
-      buildInputs = [pkgs.colmena];
-    };
-    nixosConfigurations = let
-    in {
-      vger = lib.mkHost {
-        name = "vger";
-        modules = [
-          nixos-hardware.nixosModules.lenovo-thinkpad-t480s
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              sharedModules = import ./modules;
-              users.xanderio = import ./home.nix;
-            };
-          }
-        ];
+      overlays = import ./overlays {
+        inherit pkgs inputs;
       };
+
+      pkgs = import nixpkgs {
+        inherit system overlays;
+        config.allowUnfree = true;
+      };
+      lib = import ./lib { inherit nixpkgs home-manager pkgs overlays system inputs; };
+    in
+    {
+      devShells."${system}".default = pkgs.mkShellNoCC {
+        buildInputs = [ pkgs.colmena ];
+      };
+      nixosConfigurations =
+        let
+        in
+        {
+          vger = lib.mkHost {
+            name = "vger";
+            modules = [
+              nixos-hardware.nixosModules.lenovo-thinkpad-t480s
+              home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  sharedModules = import ./modules;
+                  users.xanderio = import ./home.nix;
+                };
+              }
+            ];
+          };
+        };
+      colmena =
+        import
+          ./hive.nix
+          { inherit inputs overlays; };
     };
-    colmena =
-      import
-      ./hive.nix
-      {inherit inputs overlays;};
-  };
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -1,14 +1,17 @@
-{
-  pkgs,
-  inputs,
+{ pkgs
+, inputs
+,
 }: [
   inputs.neovim-nightly-overlay.overlay
   inputs.nix-minecraft.overlay
   inputs.fenix.overlay
-  (final: prev: let
-    nixpkgs = import inputs.iwgtk-06 {system = prev.system;};
-  in {
-    iwgtk = nixpkgs.iwgtk;
-  })
-  (import ../pkgs {})
+  inputs.polymc.overlay
+  (final: prev:
+    let
+      nixpkgs = import inputs.iwgtk-06 { system = prev.system; };
+    in
+    {
+      iwgtk = nixpkgs.iwgtk;
+    })
+  (import ../pkgs { })
 ]
