4b330cdfa8942ccc1011fe119e6ec3619e61c9c7
Alexander Sieg <alex@xanderio.de>
2022-08-26 15:39:50 +0200

    Update dracula.nvim configuration

--- home/neovim/nvim/lua/config/colorschema.lua
+++ home/neovim/nvim/lua/config/colorschema.lua
@@ -1,6 +1,10 @@
-vim.g.dracula_transparent_bg = true
+local dracula = require("dracula")
+local colors = dracula.colors()
+dracula.setup({
+  transparent_bg = true,
+  overrides = {
+    FidgetTitle = { fg = colors.green, bg = colors.bg },
+    FidgetTask = { fg = colors.purple, bg = colors.bg },
+  },
+})
 vim.cmd 'colorscheme dracula'
-vim.cmd 'hi FidgetTitle guifg=#50fa7b guibg=#282A36'
-vim.cmd 'hi FidgetTask guifg=#BD93F9 guibg=#282A36'
--- vim.cmd 'hi link NormalFloat Normal'
--- vim.cmd 'hi SignColumn guibg=NONE'
