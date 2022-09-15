81104737382eaa7151a1cd2e888411edfbf3e2d9
Alexander Sieg <alex@xanderio.de>
2022-09-15 22:07:02 +0200

    ripdrag

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -21,6 +21,7 @@
       ntfy-sh
       polymc
       bibata-cursors
+      ripdrag
     ];
   };
 
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,7 @@
 {
   nvim-ts-grammars = callPackage ./nvim-ts-grammars { };
   ferium = callPackage ./ferium { };
+  ripdrag = callPackage ./ripdrag { };
   dioxus-cli = callPackage ./dioxus-cli { };
   cargo-espflash = callPackage ./cargo-espflash { };
   cargo-espmonitor = callPackage ./cargo-espmonitor { };
