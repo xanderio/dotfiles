10200d1f04093e02e89df1d7fb081bacf69e3b2e
Alexander Sieg <alex@xanderio.de>
2022-09-27 11:53:54 +0200

    Remove packages that are now in nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,5 @@
 { callPackage, ... }:
 {
   nvim-ts-grammars = callPackage ./nvim-ts-grammars { };
-  ferium = callPackage ./ferium { };
-  ripdrag = callPackage ./ripdrag { };
   dioxus-cli = callPackage ./dioxus-cli { };
 }
