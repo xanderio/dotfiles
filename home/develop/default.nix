0ea252148d9b6ba84451dc98e51ff04ffa6b873d
Alexander Sieg <alex@xanderio.de>
2022-10-11 19:45:15 +0200

    cargo: use lld

--- home/develop/default.nix
+++ home/develop/default.nix
@@ -36,7 +36,7 @@
 
       [target.x86_64-unknown-linux-gnu]
       linker = "${pkgs.clang}/bin/clang"
-      rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
+      rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.llvmPackages.bintools}/bin/ld.lld"]
     '';
   };
 }
