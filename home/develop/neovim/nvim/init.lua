7cff27750e7ea8b93db12721ae528cf60a946d3f
Alexander Sieg <alexander.sieg@cyberus-technology.de>
2022-11-21 09:09:08 +0100

    nvim: disable eslint_d

--- home/develop/neovim/nvim/init.lua
+++ home/develop/neovim/nvim/init.lua
@@ -66,9 +66,7 @@ local null_ls = require('null-ls')
 null_ls.setup({
   sources = {
     -- js,ts,etc.
-    null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.diagnostics.eslint_d,
 
     -- shell
     null_ls.builtins.code_actions.shellcheck,
