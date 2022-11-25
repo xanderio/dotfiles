9ebada21a0404d8060c2e2df272a0548021d4238
Alexander Sieg <alex@xanderio.de>
2022-11-25 12:42:36 +0100

    use mold for rust builds

--- home/develop/default.nix
+++ home/develop/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   imports = [
     ./neovim
   ];
@@ -25,18 +25,42 @@
       DARCS_DO_COLOR_LINES = "1";
     };
 
-    file.".cargo/config".text = ''
-      [build]
-      rustc-wrapper = "${pkgs.sccache}/bin/sccache"
-      rustdocflags = "--default-theme ayu"
+    file.".cargo/config".text =
+      let
+        bintools-wrapper = "${pkgs.path}/pkgs/build-support/bintools-wrapper";
+        mold' = pkgs.symlinkJoin {
+          name = "mold";
+          paths = [ pkgs.mold ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          suffixSalt = lib.replaceStrings [ "-" "." ] [ "_" "_" ] pkgs.stdenv.targetPlatform.config;
+          postBuild = ''
+            for bin in ${pkgs.mold}/bin/*; do
+              rm $out/bin/"$(basename "$bin")"
 
-      [target.wasm32-unknown-unknown]
-      linker = "wasm-ld"
+              export prog="$bin"
+              substituteAll "${bintools-wrapper}/ld-wrapper.sh" $out/bin/"$(basename "$bin")"
+              chmod +x $out/bin/"$(basename "$bin")"
 
-      [target.x86_64-unknown-linux-gnu]
-      linker = "${pkgs.clang}/bin/clang"
-      rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.llvmPackages.bintools}/bin/ld.lld"]
-    '';
+              mkdir -p $out/nix-support
+              substituteAll "${bintools-wrapper}/add-flags.sh" $out/nix-support/add-flags.sh
+              substituteAll "${bintools-wrapper}/add-hardening.sh" $out/nix-support/add-hardening.sh
+              substituteAll "${bintools-wrapper}/../wrapper-common/utils.bash" $out/nix-support/utils.bash
+            done
+          '';
+        };
+      in
+      ''
+        [build]
+        rustc-wrapper = "${pkgs.sccache}/bin/sccache"
+        rustdocflags = "--default-theme ayu"
+
+        [target.wasm32-unknown-unknown]
+        linker = "wasm-ld"
+
+        [target.x86_64-unknown-linux-gnu]
+        linker = "${pkgs.clang}/bin/clang"
+        rustflags = ["-C", "link-arg=-fuse-ld=${mold'}/bin/mold"]
+      '';
   };
 
   xdg.configFile."pgcli/config".source = ./pgcli/config;
