c0e36decc3fc0ffa559bfebefa4f22b5d5eb347a
Alexander Sieg <alex@xanderio.de>
2022-12-23 10:36:53 +0100

    hass: fixed nginx/tailscale problem

--- hosts/delenn/hass.nix
+++ hosts/delenn/hass.nix
@@ -1,21 +1,28 @@
 { ... }: {
-  services.nginx.virtualHosts = {
-    "home.xanderio.de" = {
-      enableACME = true;
-      forceSSL = true;
-      locations =
-        let
-          upstream = "http://hal9000:8123";
-        in
-        {
-          "/" = {
-            proxyPass = upstream;
+  systemd.services.nginx = {
+    requires = [ "tailscaled.service" ];
+    after = [ "tailscaled.service" ];
+  };
+  services.nginx = {
+    validateConfig = false;
+    virtualHosts = {
+      "home.xanderio.de" = {
+        enableACME = true;
+        forceSSL = true;
+        locations =
+          let
+            upstream = "http://hal9000.tail2f592.ts.net:8123";
+          in
+          {
+            "/" = {
+              proxyPass = upstream;
+            };
+            "/api/websocket" = {
+              proxyWebsockets = true;
+              proxyPass = upstream;
+            };
           };
-          "/api/websocket" = {
-            proxyWebsockets = true;
-            proxyPass = upstream;
-          };
-        };
+      };
     };
   };
 }
