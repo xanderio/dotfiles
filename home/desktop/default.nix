e985e5c6e14bd22b804db3eb025342ecc23c580f
Alexander Sieg <alex@xanderio.de>
2022-12-25 13:15:15 +0100

    remove unneeded overlay and override

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -22,7 +22,7 @@
       ntfy-sh
       bibata-cursors
       ripdrag
-      (prismlauncher.override { jdk = pkgs.jdk17; })
+      prismlauncher
       element-desktop
     ];
   };
--- modules/minimal.nix
+++ modules/minimal.nix
@@ -4,10 +4,6 @@
     inputs.neovim-nightly-overlay.overlay
     inputs.nix-minecraft.overlay
     inputs.fenix.overlays.default
-    (final: prev: {
-      httpie = inputs.nixpkgs-master.legacyPackages.${final.stdenv.system}.httpie;
-      element-desktop = inputs.nixpkgs-master.legacyPackages.${final.stdenv.system}.element-desktop;
-    })
   ];
 
   networking.firewall.checkReversePath = false;
