1ef7dc1af64570194bf739b9e6e7fd7f59d21944
Alexander Sieg <alex@xanderio.de>
2022-12-29 11:14:02 +0100

    fonts: simplify nerd fonts

--- modules/laptop/fonts.nix
+++ modules/laptop/fonts.nix
@@ -1,19 +1,8 @@
 { pkgs, ... }:
 let
-  nerdfontSymbolsOnly2024em = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/aabed73fdd141bb8ff887b17e2acbe8187f007b8/patched-fonts/NerdFontsSymbolsOnly/complete/Symbols-2048-em%20Nerd%20Font%20Complete.ttf";
-    name = "Symbols-2048-em Nerd Font Complete.ttf";
-    sha256 = "2qhN3+yFP7k4kI4F2jBD3l2fpKGCQ8vH8hnoHh8urFk=";
-  };
-  nerdfontSymbolsOnly = pkgs.runCommand "nerdfont-symbols" { prefer-local-build = true; } ''
-    dst=$out/share/fonts/NerdFontsSymbols
-    mkdir -p $dst
-
-    ln -s "${nerdfontSymbolsOnly2024em}" "$dst/Symbols-2048-em Nerd Font Complete.ttf"
-  '';
   nerdfontConfig = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/aabed73fdd141bb8ff887b17e2acbe8187f007b8/10-nerd-font-symbols.conf";
-    sha256 = "1qy39f3idfpxrs9q61x2s3m4kz7ghqn0vhw0c2cmw0x40wxfglx1";
+    url = "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/5c5c51e7b18eb080f1fa24df9d164a4b6ff62a6c/10-nerd-font-symbols.conf";
+    sha256 = "0a9vazsv1yx01l4jrsvzmlfha76ak5rmcd7jiwls50wfd7h38iv3";
   };
   nerdfontConfigPkg =
     pkgs.runCommand "nerdfont-conf"
@@ -37,7 +26,7 @@ in
       corefonts
       freefont_ttf
       jetbrains-mono
-      nerdfontSymbolsOnly
+      (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
     ];
   };
 }
