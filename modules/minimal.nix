d2af6368ece16c570e26f81713b70617f6d56aaf
Alexander Sieg <alex@xanderio.de>
2023-01-08 12:41:44 +0100

    fish version 3.6.0

--- modules/minimal.nix
+++ modules/minimal.nix
@@ -4,6 +4,9 @@
     inputs.neovim-nightly-overlay.overlay
     inputs.nix-minecraft.overlay
     inputs.fenix.overlays.default
+    (final: prev: {
+      inherit (inputs.nixpkgs-fish-3-6-0.legacyPackages.${final.stdenv.system}) fish;
+    })
   ];
 
   networking.firewall.checkReversePath = false;
