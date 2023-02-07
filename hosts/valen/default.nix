3e68b12c3d51bd5525b38899aa65cccc8ccd2535
Alexander Sieg <alex@xanderio.de>
2023-02-07 21:49:10 +0100

    setup grocy

--- hosts/valen/default.nix
+++ hosts/valen/default.nix
@@ -6,4 +6,13 @@
     ./outline.nix
   ];
   networking.hostName = "valen";
+
+  services.grocy = {
+    enable = true;
+    hostName = "grocy.xanderio.de";
+    settings = {
+      currency = "EUR";
+      calendar.firstDayOfWeek = 1;
+    };
+  };
 }
