1d5e9512361d0d4cb0c3a42ad0394717d6a5e5b1
Alexander Sieg <alex@xanderio.de>
2023-02-21 17:27:34 +0100

    fix vcam script

--- home/shell/fish.nix
+++ home/shell/fish.nix
@@ -66,7 +66,7 @@
         body =
           let
             gphoto2 = "${pkgs.gphoto2}/bin/gphoto2";
-            ffmpeg = "${pkgs.ffmpeg}/bin/ffmpeg";
+            ffmpeg = "${pkgs.ffmpeg-full}/bin/ffmpeg";
           in
           ''
             ${gphoto2} --stdout --capture-movie | ${ffmpeg} -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
