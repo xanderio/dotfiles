5265f74072829a15a4adf0a64ad7802c10bf6128
Alexander Sieg <alex@xanderio.de>
2023-03-07 22:06:58 +0100

    nvim: change nvim-tree startup behavior

--- home/develop/neovim/nvim/lua/config/nvim-tree.lua
+++ home/develop/neovim/nvim/lua/config/nvim-tree.lua
@@ -1,3 +1,21 @@
 require('nvim-tree').setup()
 vim.keymap.set('n', '<leader>tt' , require('nvim-tree.api').tree.toggle)
 vim.keymap.set('n', '<leader>tf' , require('nvim-tree.api').tree.focus)
+
+
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
