b9d18d16a789d1aee614a7e13eeb7c0e7337273e
Alexander Sieg <alex@xanderio.de>
2023-03-15 12:23:24 +0100

    remove nil overlay

--- modules/minimal.nix
+++ modules/minimal.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }:
+{ inputs, ... }:
 {
   nixpkgs.overlays = [
     inputs.neovim-nightly-overlay.overlay
@@ -8,14 +8,6 @@
     (final: prev: {
       inherit (inputs.nixos-small.legacyPackages.${final.stdenv.system}) fish;
       inherit (inputs.nixos-small.legacyPackages.${final.stdenv.system}) bibata-cursors clickgen;
-      nil = prev.nil.overrideAttrs (old: {
-        src = prev.fetchFromGitHub {
-          owner = "oxalica";
-          repo = "nil";
-          rev = "b5797b481ae87de5d4d99791e08400b9ac5a43a5";
-          sha256 = "sha256-5ABNUoXmlGjwkqGJ3YhUeQarcu0qwH7Al2tiuR/Pzoc=";
-        };
-      });
     })
   ];
 
