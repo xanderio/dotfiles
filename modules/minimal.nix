5f667a444ea6630f900d7a2dc2104b78e1423504
Alexander Sieg <alex@xanderio.de>
2023-03-28 15:47:08 +0200

    add diffview-nvim patch for nvim nightly

--- modules/minimal.nix
+++ modules/minimal.nix
@@ -7,6 +7,17 @@
     inputs.nix-your-shell.overlays.default
     (final: prev: {
       inherit (inputs.nixpkgs-master.legacyPackages.${final.stdenv.system}) elixir-ls;
+
+      vimPlugins = prev.vimPlugins // {
+        diffview-nvim = prev.vimPlugins.diffview-nvim.overrideAttrs (old: {
+          patches = [
+            (prev.fetchurl {
+              url = "https://patch-diff.githubusercontent.com/raw/sindrets/diffview.nvim/pull/326.diff";
+              sha256 = "1zjli1m4yvwjay5qr8cnbv9kwfj724hjrc3ycaxd3iaf0afmw54s";
+            })
+          ];
+        });
+      };
     })
   ];
 
