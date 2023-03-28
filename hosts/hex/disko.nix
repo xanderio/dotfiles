9b3e9d32609870e1656ed16f3a1151d18401db5f
Alexander Sieg <alex@xanderio.de>
2023-03-28 22:02:25 +0200

    manage disk with disko on hex

---
+++ hosts/hex/disko.nix
@@ -0,0 +1,108 @@
+{ disks ? [ "/dev/vdb" ], ... }: {
+  disk = {
+    nvme0n1 = {
+      device = builtins.elemAt disks 0;
+      type = "disk";
+      content = {
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            type = "partition";
+            name = "ESP";
+            start = "1MiB";
+            end = "100MiB";
+            bootable = true;
+            content = {
+              type = "filesystem";
+              format = "fat32";
+              mountpoint = "/boot";
+            };
+          }
+          {
+            type = "partition";
+            name = "luks";
+            start = "100MiB";
+            end = "-8GiB";
+            content = {
+              type = "luks";
+              name = "crypt_root";
+              extraOpenArgs = [ "--allow-discards" ];
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          }
+          {
+            type = "partition";
+            name = "swap";
+            start = "-8GiB";
+            end = "100%";
+            content = {
+              type = "swap";
+              randomEncryption = true;
+            };
+          }
+        ];
+      };
+    };
+  };
+  zpool = {
+    zroot = {
+      type = "zpool";
+      mode = "";
+      options = {
+        ashift = "9";
+        autotrim = "on";
+      };
+      rootFsOptions = {
+        acltype = "posixacl";
+        canmount = "off";
+        compression = "zstd";
+        dnodesize = "auto";
+        normalization = "formD";
+        relatime = "on";
+        xattr = "sa";
+      };
+
+      datasets = {
+        nixos = {
+          zfs_type = "filesystem";
+          options = {
+            canmount = "off";
+            mountpoint = "none";
+          };
+        };
+        "nixos/root" = {
+          zfs_type = "filesystem";
+          mountpoint = "/";
+        };
+        "nixos/home" = {
+          zfs_type = "filesystem";
+          mountpoint = "/home";
+        };
+        "nixos/nix" = {
+          zfs_type = "filesystem";
+          mountpoint = "/nix";
+        };
+        "nixos/nix/store" = {
+          zfs_type = "filesystem";
+          mountpoint = "/nix/store";
+        };
+        "nixos/var" = {
+          zfs_type = "filesystem";
+          options.mountpoint = "none";
+        };
+        "nixos/var/lib" = {
+          zfs_type = "filesystem";
+          mountpoint = "/var/lib";
+        };
+        "nixos/var/log" = {
+          zfs_type = "filesystem";
+          mountpoint = "/var/log";
+        };
+      };
+    };
+  };
+}
