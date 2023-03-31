a6f3e6d69e4508118081a81de2beea160682ccaf
Alexander Sieg <alex@xanderio.de>
2023-03-31 14:18:48 +0200

    remove grocy

--- hosts/valen/default.nix
+++ hosts/valen/default.nix
@@ -1,18 +1,8 @@
-{ config, ... }: {
+{
   imports = [
-    ./headscale.nix
     ./configuration.nix
     ./loki.nix
     ./outline.nix
   ];
   networking.hostName = "valen";
-
-  services.grocy = {
-    enable = true;
-    hostName = "grocy.xanderio.de";
-    settings = {
-      currency = "EUR";
-      calendar.firstDayOfWeek = 1;
-    };
-  };
 }
--- hosts/valen/headscale.nix
+++
@@ -1,6 +0,0 @@
-{ config, ... }: {
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-    "net.ipv6.conf.all.forwarding" = 1;
-  };
-}
