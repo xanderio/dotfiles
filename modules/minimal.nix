dd4b0644af408b8b64fc5290c036b7bc9450bbaf
Alexander Sieg <alex@xanderio.de>
2023-04-18 10:20:30 +0200

    update xdg-desktop-portal-wlr to 0.7.0

--- modules/minimal.nix
+++ modules/minimal.nix
@@ -6,6 +6,20 @@
     inputs.fenix.overlays.default
     inputs.nix-your-shell.overlays.default
 
+    (final: prev: {
+      xdg-desktop-portal-wlr = prev.xdg-desktop-portal-wlr.overrideAttrs (oldAttrs: rec {
+        version = "0.7.0";
+        src = final.fetchFromGitHub {
+          owner = "emersion";
+          repo = "xdg-desktop-portal-wlr";
+          rev = "v${version}";
+          sha256 = "sha256-EwBHkXFEPAEgVUGC/0e2Bae/rV5lec1ttfbJ5ce9cKw=";
+        };
+
+        patches = [];
+      });
+    })
+
   ];
 
   networking.firewall.checkReversePath = false;
