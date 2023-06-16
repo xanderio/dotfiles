4e3976085063080d6920fcdb2623ca820bed6ed7
Alexander Sieg <alex@xanderio.de>
2023-06-16 12:19:25 +0200

    neotest config

--- home/develop/neovim/nvim/lua/config/neotest.lua
+++ home/develop/neovim/nvim/lua/config/neotest.lua
@@ -5,9 +5,27 @@ neotest.setup({
     require("neotest-rust"),
     require("neotest-elixir")
   },
+  quickfix = {
+    open = false
+  },
 })
 
 vim.keymap.set('n', '<leader>tr', neotest.run.run)
 vim.keymap.set('n', '<leader>tf', function() neotest.run.run(vim.fn.expand("%")) end)
 vim.keymap.set('n', '<leader>tt', neotest.summary.toggle)
 vim.keymap.set('n', '<leader>to', neotest.output.open)
+
+local group = vim.api.nvim_create_augroup("NeotestConfig", {})
+for _, ft in ipairs({ "output", "attach", "summary" }) do
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "neotest-" .. ft,
+    group = group,
+    callback = function(opts)
+      vim.keymap.set("n", "q", function()
+        pcall(vim.api.nvim_win_close, 0, true)
+      end, {
+        buffer = opts.buf,
+      })
+    end,
+  })
+end
