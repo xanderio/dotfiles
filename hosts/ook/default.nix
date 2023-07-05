03d7a91210ec07dba5ca832720b36fd645d296fa
Alexander Sieg <alex@xanderio.de>
2023-07-05 13:09:03 +0200

    ook: trusted user xanderio

--- hosts/ook/default.nix
+++ hosts/ook/default.nix
@@ -35,33 +35,51 @@
         modules = [
           inputs.home-manager.darwinModules.home-manager
           {
-            nixpkgs.overlays = [(final: prev: {
-              nix = final.nixVersions.nix_2_16;
-            })];
+            nixpkgs.overlays = [
+              (final: prev: {
+                nix = final.nixVersions.nix_2_16;
+              })
+            ];
             system.stateVersion = 4;
             programs.fish.enable = true;
             services.nix-daemon.enable = true;
-            nix.settings = {
-              experimental-features = "nix-command flakes";
-              auto-optimise-store = true;
-              extra-trusted-users = [ "xanderio" ];
-              builders-use-substitutes = true;
-              extra-nix-path = "nixpkgs=flake:nixpkgs";
+            nix = {
+              nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+              registry.nixpkgs.flake = inputs.nixpkgs;
+              settings = {
+                auto-optimise-store = true;
+                warn-dirty = false;
+                experimental-features = "nix-command flakes";
+                fallback = true;
+                connect-timeout = 5;
+                log-lines = 25;
+                trusted-users = [ "root" "xanderio" ];
+                builders-use-substitutes = true;
+
+                substituters = [
+                  "https://nix-community.cachix.org"
+                  "https://xanderio.cachix.org"
+                ];
+                trusted-public-keys = [
+                  "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                  "xanderio.cachix.org-1:MorhZh9LUPDXE0racYZBWb2JQCWmS+r3SQo4zKn51xg="
+                ];
+              };
+              distributedBuilds = true;
+              buildMachines = [{
+                sshUser = "builder";
+                hostName = "localhost";
+                systems = [
+                  linuxSystem
+                  "x86_64-linux"
+                ];
+                maxJobs = 4;
+                supportedFeatures = [ ];
+                sshKey = "/etc/nix/builder_ed25519";
+                protocol = "ssh-ng";
+                publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo";
+              }];
             };
-            nix.distributedBuilds = true;
-            nix.buildMachines = [{
-              sshUser = "builder";
-              hostName = "localhost";
-              systems = [
-                linuxSystem
-                "x86_64-linux"
-              ];
-              maxJobs = 4;
-              supportedFeatures = [ "kvm" "benchmark" "big-parallel" ];
-              sshKey = "/etc/nix/builder_ed25519";
-              protocol = "ssh-ng";
-              publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo";
-            }];
 
             launchd.daemons.darwin-builder = {
               command = "${darwin-builder.config.system.build.macos-builder-installer}/bin/create-builder";
