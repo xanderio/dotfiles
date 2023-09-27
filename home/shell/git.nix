67372aaa1d08346b97536759a1a98e882f27e8e6
Alexander Sieg <alex@xanderio.de>
2023-09-27 13:38:24 +0200

    new git aliases

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -42,6 +42,9 @@ in
         lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
         lg = lg1;
         pf = "push --force-with-lease";
+        aa = "add .";
+        ca = "commit --ammend";
+        cae = "commit --ammend --no-edit";
       };
 
       ignores = [ ".direnv" ];
