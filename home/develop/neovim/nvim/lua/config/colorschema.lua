03f1bc0e2244465d5289625166f6d4b40e73fa42
Alexander Sieg <alex@xanderio.de>
2023-12-10 13:20:26 +0100

    fix neogit colorschema

--- home/develop/neovim/nvim/lua/config/colorschema.lua
+++ home/develop/neovim/nvim/lua/config/colorschema.lua
@@ -6,6 +6,16 @@ dracula.setup({
     FidgetTitle = { fg = colors.green, bg = colors.bg },
     FidgetTask = { fg = colors.purple, bg = colors.bg },
     ['@punctuation'] = {fg = colors.fg},
+
+    NeogitDiffAdd = { fg = colors.green, bg = colors.bg },
+    NeogitDiffDelete = { fg = colors.red, bg = colors.bg },
+    NeogitDiffContext = { fg = colors.fg, bg = colors.bg },
+
+    NeogitDiffAddHighlight = { fg = colors.green, bg = colors.bg },
+    NeogitDiffDeleteHighlight = { fg = colors.red, bg = colors.bg },
+    NeogitDiffContextHighlight = { fg = colors.fg, bg = colors.bg },
+
+    NeogitCursorLine = { bg = colors.selection },
   },
 })
 vim.cmd 'colorscheme dracula'
