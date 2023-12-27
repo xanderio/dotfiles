1c0c700ee02f94d65b28a399df687204f0149a02
Alexander Sieg <alex@xanderio.de>
2023-12-27 18:22:21 +0100

    miniflux oidc

--- hosts/delenn/miniflux.nix
+++ hosts/delenn/miniflux.nix
@@ -1,19 +1,30 @@
-{ lib, ... }: {
-  services.miniflux = {
-    enable = true;
-    adminCredentialsFile = "/dev/null";
-    config = {
-      CREATE_ADMIN = lib.mkForce "0";
-      FETCH_YOUTUBE_WATCH_TIME = "on";
-      CLEANUP_REMOVE_SESSIONS_DAYS = "120";
+{ config, lib, ... }: {
+  config = {
+    x.sops.secrets."services/miniflux/env" = {};
+    
+    services.miniflux = {
+      enable = true;
+      adminCredentialsFile = config.sops.secrets."services/miniflux/env".path;
+      config = {
+        LOG_LEVEL="debug";
+        CREATE_ADMIN = lib.mkForce "0";
+        FETCH_YOUTUBE_WATCH_TIME = "1";
+        FETCH_ODYSEE_WATCH_TIME = "1";
+        CLEANUP_REMOVE_SESSIONS_DAYS = "120";
+        OAUTH2_PROVIDER = "oidc";
+        OAUTH2_CLIENT_ID = "miniflux";
+        OAUTH2_REDIRECT_URL = "https://miniflux.xanderio.de/oauth2/oidc/callback";
+        OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://sso.xanderio.de/application/o/miniflux/";
+        OAUTH2_USER_CREATION = "1";
+      };
     };
-  };
-  services.nginx.virtualHosts = {
-    "miniflux.xanderio.de" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://localhost:8080/";
+    services.nginx.virtualHosts = {
+      "miniflux.xanderio.de" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://localhost:8080/";
+        };
       };
     };
   };
