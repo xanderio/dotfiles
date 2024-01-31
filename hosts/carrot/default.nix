83d35b6f2472e7dc2b42d852daba924622c47d09
Alexander Sieg <alex@xanderio.de>
2024-01-31 22:29:38 +0100

    borgbase

--- hosts/carrot/default.nix
+++ hosts/carrot/default.nix
@@ -1,4 +1,4 @@
-{
+{config, ...}: {
   imports = [
     ../../modules/server
     ../../profiles/hetzner_vm
@@ -22,4 +22,30 @@
   services.journald.extraConfig = ''
     SystemMaxUse = 1G
   '';
+
+  x.sops.secrets = {
+    "hosts/carrot/backup/repo_key".owner = "root";
+  };
+  programs.ssh.knownHosts = {
+    "j11x0ojk.repo.borgbase.com" = {
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMS3185JdDy7ffnr0nLWqVy8FaAQeVh1QYUSiNpW5ESq";
+    };
+  };
+  services.borgbackup.jobs = {
+    borgbase = {
+      paths = [ "/var/lib" "/home" "/root" ];
+      exclude = [ "'**/.cache'" ];
+      repo = "ssh://j11x0ojk@j11x0ojk.repo.borgbase.com/./repo::root";
+      encryption = {
+        mode = "repokey-blake2";
+        passCommand = "cat ${config.sops.secrets."all/borg_backup/repo_key".path}";
+      };
+      environment = {
+        BORG_RSH = "ssh -i /etc/ssh/ssh_host_ed25519_key";
+      };
+      compression = "auto,zstd,10";
+      extraCreateArgs = "--verbose --exclude-caches --stats --checkpoint-interval 600";
+      startAt = "hourly";
+    };
+  };
 }
