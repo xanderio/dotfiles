c9f7cf5794923b7f53d0fb665673364ff395cd1b
Alexander Sieg <alex@xanderio.de>
2024-03-15 11:35:05 +0100

    nginx: per host access_log

--- modules/server/nginx.nix
+++ modules/server/nginx.nix
@@ -2,16 +2,22 @@
 , lib
 , ...
 }: {
-  networking.firewall.allowedTCPPorts =
-    lib.mkIf config.services.nginx.enable [ 80 443 ];
 
-  services = {
-    nginx = {
-      enableReload = true;
-      recommendedGzipSettings = true;
-      recommendedOptimisation = true;
-      recommendedProxySettings = true;
-      recommendedTlsSettings = true;
+  config = {
+    networking.firewall.allowedTCPPorts =
+      lib.mkIf config.services.nginx.enable [ 80 443 ];
+
+    services = {
+      nginx = {
+        enableReload = true;
+        recommendedGzipSettings = true;
+        recommendedOptimisation = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
+        appendHttpConfig = ''
+          access_log /var/log/nginx/$server_name-access.log combined;
+        '';
+      };
     };
   };
 }
