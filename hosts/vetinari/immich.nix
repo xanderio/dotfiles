215b92c9202e1834e97a26d976cde9ccca14bc93
Alexander Sieg <alexander.sieg@cyberus-technology.de>
2024-10-04 17:35:12 +0200

    immich: increase upload size to 5G

--- hosts/vetinari/immich.nix
+++ hosts/vetinari/immich.nix
@@ -12,6 +12,9 @@
         locations."/" = {
           proxyPass = "http://${config.services.immich.host}:${toString config.services.immich.port}";
           proxyWebsockets = true;
+          extraConfig = ''
+            client_max_body_size 0;
+          '';
         };
       };
     };
