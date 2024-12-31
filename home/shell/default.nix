741e0f80d5edf2576a81f627d8c69bdf6a3e0b35
Alexander Sieg <alexander.sieg@cyberus-technology.de>
2024-12-31 15:31:03 +0100

    bat use cappuchino theme

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -43,6 +43,20 @@
     jq.enable = true;
     bat = {
       enable = true;
+      config = {
+        theme = "Catppuccin Mocha";
+      };
+      themes = {
+        "Catppuccin_Mocha" = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "699f60fc8ec434574ca7451b444b880430319941";
+            hash = "sha256-6fWoCH90IGumAMc4buLRWL0N61op+AuMNN9CAR9/OdI=";
+          };
+          file = "themes/Catppuccin Mocha.tmTheme";
+        };
+      };
     };
     home-manager.enable = true;
     nix-index.enable = true;
