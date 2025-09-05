8eb955760828a8f175792a30f19e85896abf0ed5
Alexander Sieg <alex@xanderio.de>
2025-09-05 12:48:13 +0200

    immich backup

--- hosts/vetinari/immich.nix
+++ hosts/vetinari/immich.nix
@@ -1,6 +1,22 @@
 { config, ... }:
 {
   config = {
+    x.sops.secrets = {
+      "services/immich/restic/password" = {
+        owner = config.services.immich.user;
+      };
+      "services/immich/restic/s3/key_id" = { };
+      "services/immich/restic/s3/secret_key" = { };
+    };
+
+    sops.templates."immich-restic-env" = {
+      owner = config.services.immich.user;
+      content = ''
+        AWS_ACCESS_KEY_ID = ${config.sops.placeholder."services/immich/restic/s3/key_id"}
+        AWS_SECRET_ACCESS_KEY = ${config.sops.placeholder."services/immich/restic/s3/secret_key"}
+      '';
+    };
+
     services.immich = {
       enable = true;
     };
@@ -17,6 +33,32 @@
             client_max_body_size 0;
           '';
         };
+      };
+    };
+
+    services.restic.backups."immich" = {
+      initialize = true;
+      user = config.services.immich.user;
+      passwordFile = config.sops.secrets."services/immich/restic/password".path;
+      repository = "s3:https://s3.xanderio.de/immich-backup";
+      environmentFile = config.sops.templates."immich-restic-env".path;
+      paths = [
+        config.services.immich.mediaLocation
+        "/tmp/immich/backup/"
+      ];
+      backupPrepareCommand = ''
+        mkdir -p /tmp/immich/backup 
+        ${config.services.postgresql.package}/bin/pg_dump --create --clean --if-exists immich > /tmp/immich/backup.sql
+      '';
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 5"
+        "--keep-monthly 12"
+      ];
+      timerConfig = {
+        OnCalendar = "daily";
+        RandomizedDelaySec = "5h";
+        Persistent = true;
       };
     };
   };
