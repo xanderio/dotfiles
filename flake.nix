c49f29c2c14cc3abb74475982bb1746f91d3e494
Alexander Sieg <alex@xanderio.de>
2026-02-05 13:26:47 +0100

    pin virby

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
       url = "github:nix-community/authentik-nix";
       inputs."flake-parts".follows = "flake-parts";
     };
-    virby.url = "github:quinneden/virby-nix-darwin";
+    virby.url = "github:quinneden/virby-nix-darwin/be170bd7ef21ce9773e7daa646d43f5405a1bdb2";
   };
 
   outputs =
